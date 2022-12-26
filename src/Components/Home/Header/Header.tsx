20751e9946d08d977a100456c07747f46413c0b6
patrick-miharisoa <tojoniaina.patrick@futurmap.com>
2022-12-26 12:38:33 +0300

    add Home without session header

---
+++ src/Components/Home/Header/Header.tsx
@@ -0,0 +1,72 @@
+import "./Header.scss";
+import logo from "../../../Assets/Images/icon.svg";
+import rect1 from "../../../Assets/Images/Rectangle 50.svg";
+import rect2 from "../../../Assets/Images/Rectangle 51.svg";
+import vector from "../../../Assets/Images/Vector.svg";
+import search from "../../../Assets/Images/search.svg";
+import basket from "../../../Assets/Images/basket.svg";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src={logo} alt="icon" />
+        <span>Haizara</span>
+      </div>
+      <div className="explore">
+        <span className="explore_icon">
+          <div>
+            <img src={rect1} alt="rect" />
+          </div>
+          <div>
+            <img src={rect2} alt="rect" />
+          </div>
+          <div>
+            <img src={rect1} alt="rect" />
+          </div>
+        </span>
+        <span>Explore</span>
+      </div>
+      <div>
+        <ul className="links">
+          <li>
+            <a href="#">Home</a>
+            <img src={vector} alt="down-arrow" />
+          </li>
+          <li>
+            <a href="#">Courses</a>
+            <img src={vector} alt="down-arrow" />
+          </li>
+          <li>
+            <a href="#">Events</a>
+            <img src={vector} alt="down-arrow" />
+          </li>
+          <li>
+            <a href="#">Blog</a>
+            <img src={vector} alt="down-arrow" />
+          </li>
+          <li>
+            <a href="#">Pages</a>
+            <img src={vector} alt="down-arrow" />
+          </li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <form>
+          <input type="text" placeholder="What do you want to learn ?" />
+          <button type="submit">
+            <img src={search} alt="search" />
+          </button>
+        </form>
+        <div className="basket">
+          <img src={basket} alt="basket" />
+        </div>
+      </div>
+      <div className="buttons">
+        <span className="login">Login</span>
+        <span className="signup">Sign Up</span>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,8 @@
+import Header from "./Header/Header";
+import "./Home.scss";
+
+function Home() {
+    return <Header />
+}
+
+export default Home;
