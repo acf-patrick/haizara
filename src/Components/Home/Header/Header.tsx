fab5e0ddcbd0ff6717c3f40c8ec82db005c3ec5c
acf-patrick <miharisoap@gmail.com>
2022-12-26 22:00:47 +0300

    set header

--- src/Components/Home/Header/Header.tsx
+++ src/Components/Home/Header/Header.tsx
@@ -9,25 +9,25 @@ import basket from "../../../Assets/Images/basket.svg";
 function Header() {
   return (
     <div className="header flex">
-      <div className="logo flex align-center">
-        <img src={logo} alt="icon" width="50" />
-        <span className="bold">Haizara</span>
-      </div>
-      <div className="explore flex">
-        <span className="explore__icon flex">
-          <div>
-            <img src={rect1} alt="rect" />
+      <div className="left flex">
+        <div className="logo flex align-center">
+          <img src={logo} alt="icon" width="50" />
+          <span className="bold">Haizara</span>
+        </div>
+        <div className="explore flex">
+          <div className="explore__icon flex">
+            <div>
+              <img src={rect1} alt="rect" />
+            </div>
+            <div>
+              <img src={rect2} alt="rect" />
+            </div>
+            <div>
+              <img src={rect1} alt="rect" />
+            </div>
           </div>
-          <div>
-            <img src={rect2} alt="rect" />
-          </div>
-          <div>
-            <img src={rect1} alt="rect" />
-          </div>
-        </span>
-        <span>Explore</span>
-      </div>
-      <div >
+          <div>Explore</div>
+        </div>
         <ul className="links flex">
           <li>
             <a href="#">Home</a>
@@ -49,8 +49,12 @@ function Header() {
             <a href="#">Pages</a>
             <img src={vector} alt="down-arrow" />
           </li>
-          <li><a href="#">Contact</a></li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
         </ul>
+      </div>
+      <div className="right flex align-center">
         <form>
           <input type="text" placeholder="What do you want to learn ?" />
           <button type="submit">
@@ -60,10 +64,10 @@ function Header() {
         <div className="basket">
           <img src={basket} alt="basket" />
         </div>
-      </div>
-      <div className="buttons flex">
-        <span className="login">Login</span>
-        <span className="signup">Sign Up</span>
+        <div className="buttons flex">
+          <span className="login">Login</span>
+          <span className="signup">Sign Up</span>
+        </div>
       </div>
     </div>
   );
