8a90af9a77191f68b82e5880d3a41566b66caa4d
acf-patrick <miharisoap@gmail.com>
2023-01-04 15:30:00 +0300

    add popular-courses container

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -6,6 +6,27 @@ function Home() {
     return <>
         <Header />
         <Slide />
+        <main>
+					<div className="popular-courses">
+						<div className="title">
+							<div className="text">
+								<h1>explore featured courses</h1>
+								<p>10,000+ unique online course list designs</p>
+							</div>
+							<ul className="categories">
+								<li><a href="#">All</a></li>
+								<li><a href="#" className="active">Trending</a></li>
+								<li><a href="#">Popular</a></li>
+								<li><a href="#">Featured</a></li>
+							</ul>
+						</div>
+						<div className="courses">
+							<button className="left"></button>
+							<div className="list"></div>
+							<button className="right"></button>
+						</div>
+					</div>
+				</main>
     </>
 }
 
